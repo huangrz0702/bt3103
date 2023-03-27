1cc73c987f1133fb02d3115b10c361af2e5a8c1e
xiaoyugan <51486563+AmyXueYG@users.noreply.github.com>
2023-03-28 03:17:02 +0800

    search bar
    search bar, icon list

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,8 @@
 <template>
   <photo-gallery />
+  <search-bar />
   <SlidingImages />
+  
   <TrendingFood />
   <Locations />
   <RelatedLinks />
@@ -12,6 +14,7 @@ import TrendingFood from "../components/TrendingFood.vue";
 import Locations from "../components/Locations.vue";
 import RelatedLinks from "../components/RelatedLinks.vue";
 import PhotoGallery from '../components/PhotoGallery.vue';
+import SearchBar from '../components/SearchBar.vue';
 
 export default {
   name: "App",
@@ -21,6 +24,7 @@ export default {
     TrendingFood,
     Locations,
     RelatedLinks,
+    SearchBar,
   },
 };
 </script>
